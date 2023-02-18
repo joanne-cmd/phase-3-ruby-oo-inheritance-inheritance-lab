361467498b7e3ab601ee77d4f1bd5074b0be09c6
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-02-18 17:08:34 +0300

    completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative'./user.rb'
+class Student < User
+def initialize
+ @knowledge=[]
+
+end
+ def learn(s)
+  @knowledge.push(s)
+ end
+ def knowledge
+    @knowledge
+ end
 
 end
